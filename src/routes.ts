2f34b8da524a27ed4df0df2aeec31095db422309
Chris Ma <velopace7@gmail.com>
2024-11-02 02:57:57 +0800

    update detail

--- src/routes.ts
+++ src/routes.ts
@@ -29,5 +29,42 @@ router.addHandler('CATEGORY', async ({ page, enqueueLinks }) => {
 });
 
 router.addHandler('DETAIL', async ({ request, page, log }) => {
-  log.info(`Processing: ${request.url}`)
+  log.info(`Processing: ${request.url}`);
+
+  const urlPart = request.url.split('/').slice(-1); // ['sennheiser-mke-440-professional-stereo-shotgun-microphone-mke-440]
+  const manufacturer = urlPart[0].split('-')[0]; // 'sennheiser'
+
+  const title = await page.locator('.product-meta h1').textContent();
+  const sku = await page.locator('span.product-meta__sku-number').textContent();
+
+  const priceElement = page
+    .locator('span.price')
+    .filter({
+      hasText: '$'
+    })
+    .first();
+
+  const currentPriceString = await priceElement.textContent();
+  const rawPrice = currentPriceString?.split('$')[1];
+  const price = Number(rawPrice?.replace(/,/g, ''));
+
+  const inStockElement = page
+    .locator('span.product-form__inventory')
+    .filter({
+      hasText: 'In stock',
+    })
+    .first();
+
+  const inStock = (await inStockElement.count()) > 0;
+
+  const results = {
+    url: request.url,
+    manufacturer,
+    title,
+    sku,
+    currentPrice: price,
+    availableInStock: inStock,
+  };
+
+  console.log(results);
 })
